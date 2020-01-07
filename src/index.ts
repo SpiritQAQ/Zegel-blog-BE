131d4c919f8c4c836cc6e3bb7305a5d987a65004
Zegel <zegel@Zegels-MBP.local>
2020-01-07 14:44:35 +0800

    simple index.ts express code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log(1)
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+
+const PORT: any = process.env.PORT || 3000
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('hello world')
+})
+const run = () => {
+  app.listen(PORT, () => {
+    console.log(`Service is running on http://localhost:${PORT}`)
+  })
+}
+
+run()
