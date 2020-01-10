d89918357d29e53b5657dcfab7034c885258cb92
Zegel <zegel@Zegels-MBP.local>
2020-01-10 14:49:51 +0800

    model 检测test

--- src/mongoModels/User.ts
+++ src/mongoModels/User.ts
@@ -9,7 +9,13 @@ export interface IUserDocument extends Document {
 }
 
 const userSchema: Schema = new Schema({
-  username: String,
+  // model级别检测 和 validateRegisterInput的业务级别检测可以同时存在配合使用
+  username: {
+    type: String,
+    trim: true,
+    required: [true, '用户名不得为空'],
+    minlength: [5, '用户名最小长度为5个字符']
+  },
   email: String,
   password: String,
   createAt: String
